7453248f49fb088a95e1d529f5256c998fc26159
Art0Volkii <100345483+Art0Volkii@users.noreply.github.com>
2023-04-06 09:23:49 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,17 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(content_types=["new_chat_members"])
+def new_member(message):
+    name = message.new_chat_members[0].username
+    bot.send_message(message.chat.id, f"Добро пожаловать, @{name}!")
+
+@bot.message_handler(content_type=['text'])
+def check(message):
+    mm = message.chat_members.text
+    bot.send_message(message.chat.id, mm)
+
+
+bot.polling(none_stop=True)
