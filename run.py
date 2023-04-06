8a1039610db0f10f0cfb076cea7f0a37f59bf599
Art0Volkii <100345483+Art0Volkii@users.noreply.github.com>
2023-04-06 09:53:37 +0300

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(config.TOKEN)
 @bot.message_handler(content_types=["new_chat_members"])
 def new_member(message):
     name = message.new_chat_members[0].username
-    bot.send_message(message.chat.id, f"Добро пожаловать, @{name}!")
+    bot.send_message(message.chat.id, f"Ласкаво просимо, @{name}!")
 
 @bot.message_handler(content_type=['text'])
 def check(message):
